de4917b8bbd8f6c2f60d494d752cda62a3ad47b8
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-06-01 20:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code,index)=>{
+    code.addEventListener("input",()=>{
+        code.value = code.value.replace(/[^0-9]/g, "");
+
+        if(code.value!="" && index<codes.length-1){
+            codes[index+1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+
+        if (e.key === "Backspace") {
+
+            if (code.value === "" && index > 0) {
+                codes[index - 1].value = "";
+                codes[index - 1].focus();
+                e.preventDefault();
+            }
+        }
+    });
+})
